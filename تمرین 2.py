0c0fea76f21d3ab65c9b71ea06238ae823b51f89
AmirReza82 <rezaeemehramirreza@gmail.com>
2026-06-01 00:43:11 +0330

    Add files via upload

---
+++ تمرین 2.py
@@ -0,0 +1,26 @@
+
+# tamrin list (sort)
+
+jahan = ['paris' , 'london' , 'egypt' , 'spain' , 'canada']
+print(jahan)
+# sorted kardan
+"""print(sorted(jahan))
+print(jahan)
+"""
+# reverse kardan
+"""
+jahan.reverse()
+print(jahan)
+jahan.reverse()
+print(jahan)
+"""
+# sort kardan
+"""jahan.sort()
+print(jahan)
+
+jahan.sort(reverse=True)
+print(jahan)
+"""
+# len
+a = len(jahan)
+print("teadad shahrhaye mord alaghe man " , a , " ast.")
